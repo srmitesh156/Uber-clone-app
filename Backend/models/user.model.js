77ad4756aa6d8d57eb0258d7bd4c2f8e8750d3cd
srmitesh156 <mitesh.dhobi@swiftrut.com>
2024-12-21 05:49:58 +0530

    create controller and services

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
         select:false,
-        // minlength:[8,'Password must be at least 8 characters long']
+        // minlength:[6,'Password must be at least 6 characters long']
     },
     socketId:{
         type: String
